e60def08c6cf7f6850102d4edb0d8e982839f26c
Quentin Leon <qwolf1999@gmail.com>
2019-03-19 21:19:10 -0700

    Genetic algo stuff

---
+++ genetic/gene_template.lua
@@ -0,0 +1,35 @@
+local gene = {
+    ----hunt----
+    enemyHealth = %f,
+    enemyDistance = %f,
+    isUnderTower = %f,
+    weDisabled = %f,
+    eUnderTower = %f,
+    enemyNear = %f,
+    EnemyPowerful = %f,
+    EnemyWeak = %f,
+    enemyNearAndNotLevel = %f,
+    EnemyDisabled = %f,
+    punchBack = %f,
+    allyInFight = %f,
+
+    ----retreat----
+    willEnemyTowerTargetMe = %f,
+    isEnemyTowerTargetingMeNoAlly = %f,
+    hasPassiveEnemyNearby = %f,
+    hasAggressiveEnemyNearby = %f,
+    hasEnemyCreepsNearby = %f,
+    hardRetreat = %f,
+    enemyRetreat = %f,
+    FountainMana = %f,
+    AreThereDangerPings = %f,
+
+    ----farm----
+    creepsAround = %f,
+    calcEnemyCreepHealth = %f,
+    calcEnemyCreepDist = %f,
+    enemyNotLevel = %f,
+    alone = %f
+}
+
+return gene
