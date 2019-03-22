e8fcebab7859ac70a656836a40bec973a63ae0b5
Quentin Leon <qwolf1999@gmail.com>
2019-03-21 19:44:05 -0700

    Added modified server (untested)

--- genetic/genetic.go
+++ genetic/genetic.go
@@ -1,17 +1,44 @@
 package main
 
-import (
-	"fmt"
-)
-
+var gamesPerIndividual = 250
 var populationSize = 30
 var parents = 5
 
 func main() {
-	gene := GetGeneFromFile("gene.lua")
-	fmt.Println(gene)
-	MutateGeneRand(gene, .1, .1)
-	fmt.Println(gene)
-	WriteGeneToFile(gene, "result.txt")
-	fmt.Println(CalcMidFitness(1, 0.5, 0.28, 0.22, 450, 350))
+
+	/*
+		a := []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+		b := []float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+
+		c := SpliceBreed(a, b)
+
+		fmt.Println()
+		fmt.Println(a)
+		fmt.Println(b)
+		fmt.Println(c)
+
+		c = ShuffleBreed(a, b)
+
+		fmt.Println()
+		fmt.Println(a)
+		fmt.Println(b)
+		fmt.Println(c)
+
+		c = AverageBreed(a, b)
+
+		fmt.Println()
+		fmt.Println(a)
+		fmt.Println(b)
+		fmt.Println(c)
+
+		fmt.Println()
+		gene := GetGeneFromFile("gene.lua")
+		fmt.Println(gene)
+		MutateGeneRand(gene, .1, .1)
+		fmt.Println(gene)
+		WriteGeneToFile(gene, "result.txt")
+
+		fmt.Println()
+		fmt.Println(CalcMidFitness(1, 0.5, 0.28, 0.22, 450, 350))
+	*/
 }
